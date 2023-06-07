892e6432ed5520899f62e1871190cfd9c85ec225
Deivdson <94084592+Deivdson@users.noreply.github.com>
2023-06-07 15:29:21 -0300

    Update app.py

--- api_system/app.py
+++ api_system/app.py
@@ -8,8 +8,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 @app.route('/')
 def index():
     users = User.query.all()
-    result = [u.to_dict for u in users]
+    result = [u.to_dict() for u in users]
     return Response(response=json.dumps(result), status=200, content_type="application/json")
+
+@app.route('/view/<int:id>', methods=['GET'])
+def view(id):
+    row = db.session.execute("select * from user where id = %s" % id).fetchone()
+    return Response(response=json.dumps(dict(row)), status=200, content_type="application/json")
 
 @app.route('/add', methods=['POST'])
 def add():    
@@ -34,7 +39,7 @@ def edit(id):
     db.session.commit()
     return Response(response=json.dumps(user.to_dict()), status=200, content_type="application/json")
 
-@app.route('/delete/<int:id>')
+@app.route('/delete/<int:id>', methods=['GET', 'DELETE'])
 def delete(id):
     user = User.query.get(id)
     db.session.delete(user)
