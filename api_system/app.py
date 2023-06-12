09bd15c39e1e7033eb495b2ad89afd40f0c9a099
Deivdson-NESE <deivdson.sil@gmail.com>
2023-06-12 08:09:37 -0300

    Organizando arquitetura

--- api_system/app.py
+++ api_system/app.py
@@ -1,50 +1,11 @@
-from flask import Flask, render_template, request, url_for, redirect, Response
-from models import db, User
-import json
+from controllers.user import app as user_controller        
+from models.user import db
+from flask import Flask
         
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
-@app.route('/')
-def index():
-    users = User.query.all()
-    result = [u.to_dict() for u in users]
-    return Response(response=json.dumps(result), status=200, content_type="application/json")
-
-@app.route('/view/<int:id>', methods=['GET'])
-def view(id):
-    row = db.session.execute("select * from user where id = %s" % id).fetchone()
-    return Response(response=json.dumps(dict(row)), status=200, content_type="application/json")
-
-@app.route('/add', methods=['POST'])
-def add():    
-    user = User(
-        request.form['username'],
-        request.form['password'],
-        request.form['name'],
-        request.form['email']
-        )
-    db.session.add(user)
-    db.session.commit()
-    return app.response_class(response=json.dumps({'status':'sucess', 'data':user.to_dict()}), status=200, content_type="application/json")
-
-@app.route('/edit/<int:id>', methods=['PUT', 'POST'])
-def edit(id):
-    user = User.query.get(id)
-    user.username = request.form['username']
-    user.password = request.form['password']
-    user.name = request.form['name']
-    user.email = request.form['email']
-    user.email = request.form['email']
-    db.session.commit()
-    return Response(response=json.dumps(user.to_dict()), status=200, content_type="application/json")
-
-@app.route('/delete/<int:id>', methods=['GET', 'DELETE'])
-def delete(id):
-    user = User.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-    return Response(response=json.dumps(user.to_dict()), status=200, content_type="application/json")
+app.register_blueprint(user_controller, url_prefix="/user/")
 
 if __name__ == '__main__':
     db.init_app(app=app)
---
+++ api_system/models/models.py
@@ -0,0 +1,50 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password =  password 
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+    
+    def to_dict(self, columns=[]):
+        if not columns:
+            return {"id":self.id, "username":self.username, "password":self.password, "nome":self.name, "email":self.email}
+        else:
+            return{col:getattr(self, col) for col in columns}
+        
+class Lote(db.Model):
+    __tablename__ = "lotes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    endereco = db.Column(db.String, unique=True)
+    cep = db.Column(db.String)
+    m2 = db.Column(db.Integer)
+    
+
+    def __init__(self, endereco, cep, m2):
+        self.endereco = endereco
+        self.cep = cep
+        self.m2 = m2
+
+    def __repr__(self):
+        return "<Lote %r>" % self.endereco
+    
+    def to_dict(self, columns=[]):
+        if not columns:
+            return {"id":self.id, "endereço":self.endereco, "cep":self.cep, "m²":self.m2}
+        else:
+            return{col:getattr(self, col) for col in columns    }
