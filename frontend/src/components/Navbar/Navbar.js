6c2fe75122e35aef1b21ccd8b92f4d6e171ac8f9
Aluno <lucasmelonds@gmail.com>
2023-06-21 09:57:15 -0300

    fix: Dropdown menu

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,11 +2,49 @@ import './style.css'
 
 const Navbar = () => {
     const idNavbar = localStorage.getItem('token')
+    const user = localStorage.getItem('username')
     console.log(idNavbar)
 
 	function Sair() {
 		localStorage.removeItem('token')
 	}
+
+    const outsideClick = (element, events, callback) => {
+        const html = document.documentElement;
+        const outside = 'data-outside'
+        if(!element.hasAttribute(outside)) {
+          events.forEach(userEvent => {
+            setTimeout(() => html.addEventListener(userEvent, handleOutsideClick))
+          })
+          element.setAttribute(outside, '')
+        }
+        function handleOutsideClick(event) {
+          if(!element.contains(event.target)){
+            element.removeAttribute(outside)
+            events.forEach(userEvent => {
+              html.removeEventListener(userEvent, handleOutsideClick)
+            })
+            callback();
+          }
+        }
+      }
+
+    const initDropDownMenu = () => {
+        const dropdownMenus = document.querySelectorAll('.dropdown');
+        
+        dropdownMenus.forEach(menu => {
+            ['click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick)
+            })
+        })
+        
+        function handleClick() {
+            this.classList.add('active')
+            outsideClick(this, ['click'], () => {
+            this.classList.remove('active')
+            })
+        }
+    }
 
 	const UserMenu = () => {
 		if (idNavbar == null) {
@@ -28,13 +66,14 @@ const Navbar = () => {
                     <h4><a href="/">Lotes</a></h4>
                     <h4><a href="/">Casas</a></h4>
                     <div className='login-menu'>
-                        <label htmlFor="touch"><h4>Olá</h4></label>               
-                        <input type="checkbox" id="touch" />
-                        <ul className="slide">
-                            <li><a href="/adicionar-lote">Adicionar lote</a></li> 
-                            <li><a href="/adicionar-casa">Adicionar casa</a></li>
-                            <li><a href="/" onClick={() => Sair()}>Sair</a></li>
-                        </ul>
+                        <li className="dropdown">
+                            <h4 onClick={() => initDropDownMenu()}>Olá, {user}</h4>
+                            <ul className="dropdown-menu">
+                                <li><a href="/adicionar-lote">Adicionar lote</a></li> 
+                                <li><a href="/adicionar-casa">Adicionar casa</a></li>
+                                <li><a href="/" onClick={() => Sair()}>Sair</a></li>
+                            </ul>
+                        </li>
                     </div>
                 </div>
 			);
